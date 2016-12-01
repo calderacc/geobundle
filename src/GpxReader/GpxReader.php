809411cc787d407c00b4dca30adfccfa924950c2
Malte Hübner <maltehuebner@gmx.org>
2016-12-01 18:10:31 +0100

    Start with two loading methods.

--- src/GpxReader/GpxReader.php
+++ src/GpxReader/GpxReader.php
@@ -10,10 +10,25 @@ class GpxReader
     /** @var \SimpleXMLElement $rootNode */
     protected $rootNode;
 
-    public function __construct(string $xmlString)
+    public function __construct()
+    {
+    }
+
+    public function loadFromString(string $xmlString): GpxReader
     {
         $this->xmlString = $xmlString;
 
         $this->rootNode = new \SimpleXMLElement($this->xmlString);
+
+        return $this;
+    }
+
+    public function loadFromFile(string $filename): GpxReader
+    {
+        $this->xmlString = file_get_contents($filename);
+
+        $this->rootNode = new \SimpleXMLElement($this->xmlString);
+
+        return $this;
     }
 }
