2c6d9096e9c704439844fbaa6de13f6044645d96
Malte Hübner <maltehuebner@gmx.org>
2016-12-01 17:43:01 +0100

    AbstractDistanceCalculator now handles also TrackInterface entities.

--- src/DistanceCalculator/AbstractDistanceCalculator.php
+++ src/DistanceCalculator/AbstractDistanceCalculator.php
@@ -3,6 +3,8 @@
 namespace Caldera\GeoBundle\DistanceCalculator;
 
 use Caldera\GeoBasic\Coord\CoordInterface;
+use Caldera\GeoBasic\PolylineConverter\PolylineConverter;
+use Caldera\GeoBasic\TrackInterface\TrackInterface;
 
 abstract class AbstractDistanceCalculator implements DistanceCalculatorInterface
 {
@@ -16,6 +18,22 @@ abstract class AbstractDistanceCalculator implements DistanceCalculatorInterface
     public function addCoord(CoordInterface $coord): DistanceCalculatorInterface
     {
         array_push($this->coordList, $coord);
+
+        return $this;
+    }
+
+    public function addCoords(array $coordList): DistanceCalculatorInterface
+    {
+        $this->coordList = array_merge($this->coordList, $coordList);
+
+        return $this;
+    }
+
+    public function addTrack(TrackInterface $track): DistanceCalculatorInterface
+    {
+        $coordList = PolylineConverter::getCoordList($track);
+
+        $this->addCoords($coordList);
 
         return $this;
     }
