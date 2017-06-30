c5476992914f9971683678456f8b2dacc503fa4a
Malte Hübner <maltehuebner@gmx.org>
2017-06-30 16:15:33 +0200

    Add DependencyInjection.

---
+++ DependencyInjection/CalderaGeoBundleExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Caldera\GeoBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CalderaGeoBundleExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Caldera\GeoBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+    }
+}
